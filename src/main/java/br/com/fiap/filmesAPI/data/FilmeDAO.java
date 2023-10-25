b6c5065050b367a03cc8197f9ffd2cf9385775b7
LuisMZampar <rm550531@fiap.com.br>
2023-10-24 21:31:43 -0300

    Modificando o FilmeDAO e alterando Filme.java

--- src/main/java/br/com/fiap/filmesAPI/data/FilmeDAO.java
+++ src/main/java/br/com/fiap/filmesAPI/data/FilmeDAO.java
@@ -6,13 +6,28 @@ import java.util.List;
 import br.com.fiap.filmesAPI.model.Filme;
 
 public class FilmeDAO {
-	
+
 	List<Filme> filmes = new ArrayList<>();
 
-	public List<Filme> findAll() {
-		
-		return null;
+	public FilmeDAO() {
+		filmes = List.of(
+				new Filme(1, "Mega Tubarão", "Filme de tubarão", 120, "David Backhan"),
+				new Filme(2, "As Branquelas", "Filme de brancos", 140, "Keenem ivory Wayans")
+
+		);
 	}
-	
-	
+
+	public List<Filme> findAll() {
+		return filmes;
+	}
+
+	public Filme findById(Long id) {
+		return filmes
+				.stream()
+				.filter(filme -> filme.id() == id)
+				.findFirst()
+				.orElse(null);
+		
+	}
+
 }
--- src/main/java/br/com/fiap/filmesAPI/model/Filme.java
+++ src/main/java/br/com/fiap/filmesAPI/model/Filme.java
@@ -1,10 +1,10 @@
 package br.com.fiap.filmesAPI.model;
 
-public class Filme {
-	Long id;
-	String nome;
-	String descricao;
-	int tempo_duracao;
-	String diretor;
+public record Filme (
+	int id,
+	String nome,
+	String descricao,
+	int tempo_duracao,
+	String diretor
 	
-}
+){}
